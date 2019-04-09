bd0b3b273bdc66cd453d8c4fe43fc423c45b73d9
Jose Pablo <jop0693@gmail.com>
2019-04-08 19:25:58 -0500

    finally working

--- server/index.js
+++ server/index.js
@@ -6,11 +6,10 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-
 app.use(cors());
 app.use(bodyParser.json());
 
-// Postgress Client Setup
+// Postgres Client Setup
 const { Pool } = require('pg');
 const pgClient = new Pool({
   user: keys.pgUser,
@@ -19,30 +18,30 @@ const pgClient = new Pool({
   password: keys.pgPassword,
   port: keys.pgPort
 });
-
 pgClient.on('error', () => console.log('Lost PG connection'));
 
-pgClient.query('CREATE TABLE IF NOT EXISTS values (number INT)')
-  .catch((err) => console.log(err));
+pgClient
+  .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+  .catch(err => console.log(err));
 
 // Redis Client Setup
 const redis = require('redis');
 const redisClient = redis.createClient({
-  hosts: keys.redisHost,
+  host: keys.redisHost,
   port: keys.redisPort,
   retry_strategy: () => 1000
 });
-
 const redisPublisher = redisClient.duplicate();
 
 // Express route handlers
 
 app.get('/', (req, res) => {
   res.send('Hi');
-})
+});
 
 app.get('/values/all', async (req, res) => {
-  const values = await pgClient.query('SELECT * FROM values');
+  const values = await pgClient.query('SELECT * from values');
+
   res.send(values.rows);
 });
 
@@ -56,18 +55,16 @@ app.post('/values', async (req, res) => {
   const index = req.body.index;
 
   if (parseInt(index) > 40) {
-    return res.status(422).send('Index too high for the low budget we have!');
+    return res.status(422).send('Index too high');
   }
 
   redisClient.hset('values', index, 'Nothing yet!');
   redisPublisher.publish('insert', index);
-
-  pgClient.query('INSERT INTO values(number) VALUES ($1)', [index]);
+  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
 
   res.send({ working: true });
-})
+});
 
 app.listen(5000, err => {
-  if (err) console.log(err);
-  console.log('Server listening port 5000')
+  console.log('Listening');
 });
